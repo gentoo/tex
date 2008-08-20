667f1cbe6fbc8f5ceed064d3b2ddf37c4a742f75
Alexis Ballier <aballier@gentoo.org>
2008-08-20 08:52:30 +0000

    map catalogue licenses to ours
    svn path=/; revision=298

---
+++ scripts/texlive/tlpsrc/cataloguelicense2gentoolicense.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+while [ $# -gt 0 ] ; do
+    case "$1" in
+        gpl)
+            echo "GPL-1";;
+        gpl2)
+            echo "GPL-2";;
+        lppl)
+            echo "LPPL-1.2";;
+        lppl1.2)
+            echo "LPPL-1.2";;
+        lppl1.3)
+            echo "LPPL-1.3";;
+        gfl)
+            echo "LPPL-1.3";;
+        gfsl)
+            echo "LPPL-1.3";;
+        unknown)
+            echo "freedist";;
+        other-free)
+            echo "as-is";;            
+        other)
+            echo "as-is";;
+        lgpl)
+            echo "LGPL-2";;
+        lgpl2.1)
+            echo "LGPL-2.1";;
+        pd)
+            echo "public-domain";;
+        apache2)
+            echo "Apache-2.0";;
+        artistic2)
+            echo "Artistic-2";;
+        artistic)
+            echo "Artistic";;
+        bsd)
+            echo "BSD";;
+        knuth)
+            echo "TeX";;
+        fdl)
+            echo "FDL-1.1";;
+        ofl)
+            echo "OFL";;
+        nosell)
+            echo "freedist";;
+        *)
+            echo "The $1 license is not mapped yet!"
+            exit 1;;
+    esac
+    shift
+done
